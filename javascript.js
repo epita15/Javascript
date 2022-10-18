66b3d31a889ed506d347d36386452f170042b75b
epita15 <valeri.epitashvili241@eab.tsu.edu.ge>
2022-10-18 19:10:17 +0400

    javascript davaleba1

---
+++ javascript.js
@@ -0,0 +1,109 @@
+
+//  1
+// let user = {
+//     firstname: "giorgi" ,
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+// }
+
+
+// console.log(user.studentstatus);
+
+
+
+//  2
+// let names = ["ana" , "beqa", "giorgi", "levani", "koba"];
+
+// for (let x = 0; x < names.length; x++){
+//     console.log(names[x]);
+// }
+// for (let y of names) {
+//     console.log(y);
+// }
+
+
+// let z = 0;
+// while(z < names.length) {
+//     console.log(names[z]);
+//     z++;
+// }
+
+// 3
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for ( let i = 0; i < numbers.length; i++) {
+//     if(numbers[i] > 5 ) { 
+//     console.log(numbers[i]);
+// }
+// }
+
+// for(let item of numbers){
+//     if (item > 5) {
+//         console.log(item);
+//     }
+// }
+
+
+// 4
+
+
+// let user = {
+//     name : "giorgi",
+//     age: 20,
+//     studentstatus: "active"
+// }
+
+// if (user.age < 18 && user.studentstatus == "active" ) {
+//     console.log("Hello");
+// } else if (user.name == "levani") {
+//     console.log("hello levani");
+// } else if (user.studentstatus == "active" || user.age < 25) {
+//     console.log("hello world");
+// } else {
+//     console.log("error");
+// }
+
+
+
+//  5
+
+// let array = ["watermelon" , "pear" , 10, 45, 50, "apple", "ananas" ];
+
+// console.log(array[0], array[1], array[5], array[6], );
+
+// 6
+
+// let array = [ 2, -3, 5, 10, 25, -24, -11, 100 , -6, -7, 10 ]
+// for ( let item of array) {
+//     if (item > 0) {
+//     console.log(item);
+//     }
+// }
+
+// 7
+
+// let array = [ 2, 3, 5, 10, 25, 24, 11, 100 , 6, 7, 10 ]
+
+// for (let item of array) {
+//     if ( item % 2 == 0) {
+//         console.log(item);
+//     }
+// }
+
+// for (let item of array) {
+//     if ( item % 2 == 1) {
+//         console.log(item);
+//     }
+// }
+
+
+// 8
+
+
+// let users = [
+//     {username: "giorgi", status: false},
+//     {username: "levani", status: false},
+//     {username: "anna", status: true}
+// ]
